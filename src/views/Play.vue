3a52a1eea93b8c227e29402275ec21cc6b8eedff
charlesarnould <charlesarnould60@gmail.com>
2021-06-24 15:37:46 +0200

    début classe Play, fatigue ++

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,15 +1,23 @@
 <template>
-  <div id="rectangle">
-    <p>Etape n°2</p>
-
-    <div id="grandRectangle">
-      <div class="home_choix_button">
-        <router-link to="/Test1">
-          <button>
-            <b>Maintenez le bouton appuyé et toussez :')</b>
-          </button>
-        </router-link>
+  <div class="Play">
+    <div class="play_etape2">
+      <p><b>Etape n°2</b></p>
+    </div>
+    <div class="gros_rectangle">
+      <div class="rond_micro">
+        <div class="micro">
+          <router-link to="/Replay">
+            <button>
+              <img
+                src="https://e7.pngegg.com/pngimages/135/8/png-clipart-microphone-computer-icons-microphone-electronics-microphone-thumbnail.png"
+              />
+            </button>
+          </router-link>
+        </div>
       </div>
+    </div>
+    <div class="texte_micro">
+      <p><b>Maintenez le micro appuyé et toussez</b></p>
     </div>
   </div>
 </template>
@@ -19,15 +27,64 @@
 
 
 <style scoped>
-#rectangle {
-  position: absolute;
-  width: 300px;
-  height: 264px;
-  box-shadow: 0px 2px 3px #2e3244;
-  background: #f1895c;
+#Play {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  width: 100vw;
+  flex-direction: column;
 }
 
-#grandRectangle {
+#play_etape2 {
+  width: 25vw;
+  height: 7vh;
+  background: #f1895c;
+  box-shadow: 6px 8px 8px #2e3244;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+}
+
+#micro {
+  position: absolute;
+  width: 135px;
+  height: 132px;
+  left: 668px;
+  top: 363px;
+
+  background: #516079;
+}
+
+#rond_micro  {
+  position: absolute;
+  width: 135px;
+  height: 132px;
+  left: 668px;
+  top: 363px;
+
+  background: #516079;
+}
+
+#texte_micro {
+  position: absolute;
+  width: 600px;
+  height: 50px;
+  left: 433px;
+  top: 545px;
+
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+
+  color: #2e3244;
+}
+
+#gros_rectangle {
   position: absolute;
   width: 1266px;
   height: 281px;
---
+++ src/views/Replay.vue
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+    <div class="play_choix_button_2">
+      <router-link to="/Play">
+        <button>
+          <b>Réenregistrer ma toux</b>
+        </button>
+      </router-link>
+    </div>
+    
